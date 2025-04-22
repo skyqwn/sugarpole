8000b21202ef872f2ff6f7c461def94e35769cfb
kimba <skyqwn@gmail.com>
2025-04-22 17:47:51 +0900

    feat: Button Component add & header style ui add

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import "./App.css";
+import Button from "./components/common/Button";
 
-function App() {
+export default function App() {
   return (
-    <>
-      <h2 className="text-red-500">test</h2>
-    </>
+    <div className="w-[1440px] h-screen mx-auto">
+      <header className="flex items-center px-10 py-4 w-full h-[116px] justify-between">
+        <h1 className="text-xl font-bold leading-[110%]">
+          Dicom Viewer(with Cornstone.js)
+        </h1>
+
+        <div className="flex items-center w-[1014px] justify-between">
+          <ul className=" flex gap-6 w-[649px] justify-between py-3 px-2">
+            <li>Zoom</li>
+            <li>Flip H</li>
+            <li>Flip V</li>
+            <li>Rotate Delta 30</li>
+            <li>Invert</li>
+            <li>Apply Colormap</li>
+            <li>Reset</li>
+          </ul>
+          <Button>Previous Image</Button>
+          <Button>Next Image</Button>
+        </div>
+      </header>
+    </div>
   );
 }
-
-export default App;
---
+++ src/components/common/Button.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+type ButtonVariant = "primary" | "secondary";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: ButtonVariant;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  children,
+  variant = "primary",
+  className = "",
+  ...rest
+}) => {
+  const baseClasses =
+    "h-12 px-3 py-4 border-2 text-base font-medium transition-colors duration-200 flex items-center cursor-pointer";
+
+  const variantClasses =
+    variant === "secondary"
+      ? "text-black bg-transparent  hover:bg-gray-200 border-transparent"
+      : "text-white bg-[#0F62FE] hover:bg-[#0053D6] border-[#0F62FE]";
+
+  return (
+    <button
+      className={`${baseClasses} ${variantClasses} ${className} `}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
